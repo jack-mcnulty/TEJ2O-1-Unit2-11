3344ada370b7f67ca75e7f9fe3ac7fd49c59feee
jack-mcnulty <jack.mcnulty@stu.ocsb.ca>
2025-10-21 13:29:23 -0400

    compares two numbers

--- main.ts
+++ main.ts
@@ -1,8 +1,42 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jack McNulty
+ * Created on: Oct 2025
+ * This program compares two numbers
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// variables
+let number1: number = 0
+number1 = randint (0,99)
+let number2: number = 0
+number2 = randint (0,99)
+
+// button A 
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    basic.showString('#1 :')
+    basic.showNumber(number1)
+})
+
+// button B
+input.onButtonPressed(Button.B, function() {
+    basic.clearScreen()
+    basic.showString('#2 :')
+    basic.showNumber(number2)
+})
+
+// comparing numbers
+input.onGesture(Gesture.Shake, function() {
+    if (number1 >= number2)
+    basic.showString(number1.toString() + ">" + number2.toString())
+} else 
+    basic.showString(number1.toString() + ">" + number2.toString())
+
+// pause and show you're ready again 
+basic.pause(1000)
+basic.showIcon(IconNames.Sad)
+)
